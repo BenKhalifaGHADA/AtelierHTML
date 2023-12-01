ab50394912c865e6a41c8320de6b520cc3d8bea6
Ben khalifa Ghada <benkhalifaghada@gmail.com>
2023-12-01 09:48:14 +0100

    Seance

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//  alert("Hello 3B");
+           /* prompt("Voulez vous se desinscrire");
+            confirm("Voulez vous supprimer un utilisateur");*/
+           /* console.log("Hello console ");
+            var a=500;
+            console.log(a);
+            alert("a"+a);
+            let b;
+
+            const PI=3.14;
+            console.log(PI);*/
+
+            if(true){
+                var z=2020;
+            }
+            console.log(z);
+            if(true){
+                var y="Hello!";
+            }
+
+            console.log(y);
+            var ch="2023"; //typage dynamique 
+            console.log(typeof ch);
+            ch=123;
+            console.log(typeof ch);
+            ch=true;
+            console.log(typeof ch);
